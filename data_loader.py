f99fb45602e971c851f7ed6e385c79cccbdbc430
NetoPedro <pedroneto_09@hotmail.com>
2020-07-03 15:38:34 +0100

    Added structure of the classes for the Cityscape and BDD100k datasets

---
+++ data_loader.py
@@ -0,0 +1,25 @@
+import torch
+from torch import nn
+from torch.utils.data import Dataset
+
+class CityscapeDataset(Dataset):
+
+    def __init__(self):
+        pass
+
+    def __getitem__(self, item):
+        pass
+
+    def __len__(self):
+        pass
+
+class Bdd100kDataset(Dataset):
+
+    def __init__(self):
+        pass
+
+    def __getitem__(self, item):
+        pass
+
+    def __len__(self):
+        pass
