7483edb31df68a8018a096f932f42e63141245e0
NetoPedro <pedroneto_09@hotmail.com>
2020-07-05 17:32:42 +0100

    Added classes for the models to be used in this project

---
+++ models.py
@@ -0,0 +1,44 @@
+import torch
+from torch import nn
+
+
+class UNet(nn.Module):
+
+    def __init__(self):
+        pass
+
+    def forward(self,x):
+        pass
+
+class FastFCN(nn.Module):
+
+    def __init__(self):
+        pass
+
+    def forward(self,x):
+        pass
+
+class GatedSCNN(nn.Module):
+
+    def __init__(self):
+        pass
+
+    def forward(self,x):
+        pass
+
+class DeepLab(nn.Module):
+
+    def __init__(self):
+        pass
+
+    def forward(self,x):
+        pass
+
+class MaskRCNN(nn.Module):
+
+    def __init__(self):
+        pass
+
+    def forward(self,x):
+        pass
+
