d36c83a5460d745411efed49a789e115ae136fb9
NetoPedro <pedroneto_09@hotmail.com>
2020-12-30 04:35:39 +0000

    Added the decoder part of the UNet

--- models.py
+++ models.py
@@ -2,43 +2,66 @@ import torch
 from torch import nn
 
 
+class UNetBlockA(nn.Module):
+    def __init__(self,channel_in,channel_out):
+        self.sequential1 = torch.nn.Sequential(
+            nn.Conv2d(channel_in,channel_out,3)
+            nn.ReLU()
+            nn.Conv2d(channel_out,channel_out,3)
+            nn.ReLU()
+            nn.Conv2d(channel_out,channel_out,3)
+            nn.ReLU()
+        )
+    def forward(self,x):
+        return self.sequential1(x)
+
 class UNet(nn.Module):
 
-    def __init__(self):
-        pass
+    def __init__(self,output_dim=1):
+        self.sequential1 = torch.nn.Sequential(
+            nn.Conv2d(3,64,3)
+            nn.ReLU()
+            nn.Conv2d(64,64,3)
+            nn.ReLU()
+            nn.Conv2d(64,64,3)
+            nn.ReLU()
+        )
+        self.sequential2 = torch.nn.Sequential(
+            nn.Conv2d(64,128,3)
+            nn.ReLU()
+            nn.Conv2d(128,128,3)
+            nn.ReLU()
+            nn.Conv2d(128,128,3)
+            nn.ReLU()
+        )
 
-    def forward(self,x):
-        pass
+        self.sequential3 = torch.nn.Sequential(
+            nn.Conv2d(64,128,3)
+            nn.ReLU()
+            nn.Conv2d(128,128,3)
+            nn.ReLU()
+            nn.Conv2d(128,128,3)
+            nn.ReLU()
+        )
 
-class FastFCN(nn.Module):
+        self.sequential4 = torch.nn.Sequential(
+            nn.Conv2d(64,128,3)
+            nn.ReLU()
+            nn.Conv2d(128,128,3)
+            nn.ReLU()
+            nn.Conv2d(128,128,3)
+            nn.ReLU()
+        )
 
-    def __init__(self):
-        pass
-
-    def forward(self,x):
-        pass
-
-class GatedSCNN(nn.Module):
-
-    def __init__(self):
-        pass
-
-    def forward(self,x):
-        pass
-
-class DeepLab(nn.Module):
-
-    def __init__(self):
-        pass
-
-    def forward(self,x):
-        pass
-
-class MaskRCNN(nn.Module):
-
-    def __init__(self):
-        pass
-
+        self.sequential5 = torch.nn.Sequential(
+            nn.Conv2d(64,128,3)
+            nn.ReLU()
+            nn.Conv2d(128,128,3)
+            nn.ReLU()
+            nn.Conv2d(128,128,3)
+            nn.ReLU()
+        )
+        
     def forward(self,x):
         pass
 
